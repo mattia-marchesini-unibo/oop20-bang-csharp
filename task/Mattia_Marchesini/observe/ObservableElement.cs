a2ea2d717ab45d02f22e7e616ff6e103b2896bb3
Mattia Marchesini <mattia.marhesini5@studio.unibo.it>
2021-05-05 16:53:41 +0200

    first observe package implementation

---
+++ task/Mattia_Marchesini/observe/ObservableElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task.Mattia_Marchesini.observe
+{
+    class ObservableElement<E> : IObservable
+    {
+        protected List<Observer> Observers { get; } = new List<Observer>();
+        private E element;
+
+        public E Element
+        {
+            get { return element; }
+            set
+            {
+                this.element = value;
+                this.Observers.ForEach(delegate (Observer obs)
+                {
+                    obs();
+                });
+            }
+        }
+
+        public ObservableElement(E element) => this.element = element;
+
+        public ObservableElement() { }
+
+        public void AddObserver(Observer observer) => this.Observers.Add(observer);
+
+        public void RemoveObserver(Observer observer) => this.Observers.Remove(observer);
+
+        public void RemoveAllObservers() => this.Observers.Clear();
+
+        public void NotifyObservers()
+        {
+            this.Observers.ForEach(delegate (Observer obs)
+            {
+                obs();
+            });
+        }
+    }
+}
