c16f48b3b260191182d33a9713d6f96093ce1f3e
Mattia Marchesini <mattia.marhesini5@studio.unibo.it>
2021-05-08 14:47:47 +0200

    implement JSONDeckReader. add deck.json, TurnObservable

--- task/Mattia_Marchesini/deck/JSONDeckReader.cs
+++ task/Mattia_Marchesini/deck/JSONDeckReader.cs
@@ -1,17 +1,41 @@
 ﻿using model;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace task.Mattia_Marchesini.deck
 {
     public class JSONDeckReader : IDeckReader
     {
 
-        public JSONDeckReader() { }
+        private const string DECK_FILE = "deck.json";
+        private List<Card> cards = null;
+
+		public class JSONCard
+        {
+			public string localName { get; set; }
+			public string realName { get; set; }
+			public string color { get; set; }
+			public int howMany { get; set; }
+		}
 
         public List<Card> ReadCards()
         {
-            throw new NotImplementedException();
+			if (this.cards == null)
+			{
+				this.cards = new List<Card>();
+				List<JSONCard> c = JsonConvert.DeserializeObject<List<JSONCard>>(File.ReadAllText(DECK_FILE));
+
+				c.ForEach(crd =>
+				{
+					for (int i = 0; i < crd.howMany; i++)
+					{
+						this.cards.Add(new Card(crd.realName + i.ToString(), crd.color, crd.localName, crd.realName));
+					}
+				});
+			}
+			return cards;
         }
     }
 }
---
+++ task/Mattia_Marchesini/observe/TurnObservable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task.Mattia_Marchesini.observe
+{
+    class TurnObservable<E> : ObservableElement<E>
+    {
+        public override void NotifyObservers()
+        {
+            base.NotifyObservers();
+            this.RemoveAllObservers();
+        }
+    }
+}
