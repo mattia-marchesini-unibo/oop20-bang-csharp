87e39914b6eb117c128484e5a9010e97d37e8428
HDavi <davide.merli5@studio.unibo.it>
2021-05-08 11:38:46 +0200

    Add interface ITable and class Table

---
+++ task/Davide_Merli/model/ITable.cs
@@ -0,0 +1,31 @@
+﻿using model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+///     An interface implementing a game table.
+///     The interface contains functions to handle the players and the deck.
+/// </summary>
+/// 
+/// <author>Davide Merli</author>
+namespace task.Davide_Merli.model
+{
+    public interface ITable
+    {
+        enum Message
+        {
+            Choose_Player,
+            Choose_Player_With_Distance,
+            Choose_Card
+        }
+
+        void DiscardCard(Card card);
+
+        void RemovePlayer(Player player);
+
+        void NextPlayer();
+
+
+    }
+}
