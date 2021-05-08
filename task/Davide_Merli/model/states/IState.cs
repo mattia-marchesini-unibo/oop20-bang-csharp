aea592b5327eefa349af09426ce30a62a85afa38
HDavi <davide.merli5@studio.unibo.it>
2021-05-08 15:48:32 +0200

    Add documentation

--- task/Davide_Merli/model/states/IState.cs
+++ task/Davide_Merli/model/states/IState.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// Interface that implements a game state.
+/// </summary>
+/// 
+/// <author>Davide Merli</author>
 namespace model.states
 {
     public interface IState
     {
+        /// <summary>
+        /// Performs actions.
+        /// </summary>
+        /// 
+        /// <param name="gsMachine">the component which handles the states</param>
         void handle(GameStateMachine gsMachine);
     }
 }
